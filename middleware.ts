3b3645932741c58b797ff960d0843d8bda8c0476
shubhambhojane98 <shubhambhojane90@gmail.com>
2024-08-22 15:28:25 +0530

    fix 404 page when signout from page

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,26 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const  isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware((auth,req) => {
-    if(isProtectedRoute(req)){
-        auth().protect()
-    }
+// export default clerkMiddleware((auth,req) => {
+//     if(isProtectedRoute(req)){
+//         auth().protect()
+//     }
+// });
+
+const isPublicRoute = createRouteMatcher(["/"]);
+// const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+export default clerkMiddleware((auth, req) => {
+  if (!auth().userId && !isPublicRoute(req)) {
+    return auth().redirectToSignIn();
+  }
 });
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
