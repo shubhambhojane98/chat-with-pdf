dc7a85162e3de6e86f007130de77e2c789882ce5
shubhambhojane98 <shubhambhojane90@gmail.com>
2024-09-07 14:56:58 +0530

    footer issue solved

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,7 +9,6 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
       <div className="flex-1 flex flex-col h-screen">
         <Header />
         <main className="flex-1 overflow-y-auto">{children}</main>
-        <Footer />
       </div>
     </ClerkLoaded>
   );
