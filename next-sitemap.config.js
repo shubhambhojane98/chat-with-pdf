01adfa9d94b7743d793e0ed31c8f31090bd0d450
shubhambhojane98 <shubhambhojane90@gmail.com>
2024-09-13 16:22:28 +0530

    updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-  siteUrl: "https://www.pdfdialogue.com",
-  generateRobotsTxt: true, // Generates a robots.txt file
+  siteUrl: "https://www.pdfdialogue.com", // Your domain
+  generateRobotsTxt: true, // Generates robots.txt file
   sitemapSize: 7000, // Optional: Maximum number of URLs per sitemap file
-  outDir: "./public", // Output directory for the sitemap
+  outDir: "./public", // Output directory for sitemap files
+  generateIndexSitemap: true, // Generates sitemap index file
 };
